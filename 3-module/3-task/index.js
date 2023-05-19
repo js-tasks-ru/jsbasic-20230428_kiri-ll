adadaf596b03ff58f5cb40b10c76c00ce6354d1a
Kirill Porozov <kipor36@yandex.ru>
2023-05-19 20:16:11 +0500

    3 module (1,2,3 tasks). And 4 module (All tasks).

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,18 @@
 function camelize(str) {
-  // ваш код...
+
+	let arrStr = str.split('');
+
+	let index = arrStr.indexOf('-');
+
+	while (index !== -1) {
+
+		let upper = arrStr[index + 1].toUpperCase();
+
+		arrStr.splice(index, 2, upper);
+
+		index = arrStr.indexOf('-');
+
+	};
+
+	return arrStr.join('');
 }
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+	let arrNames = friends.map(item => `${item.firstName} ${item.lastName}`)
+	let elemUl = document.createElement('ul');
+
+	for (let name of arrNames) {
+
+		let elemLi = document.createElement('li');
+		elemLi.innerHTML = name;
+		elemUl.appendChild(elemLi);
+	};
+
+	return elemUl;
 }
