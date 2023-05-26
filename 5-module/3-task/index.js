88abe828f16f1d2c23d604b1289ebefdc6af36fc
Kirill Porozov <kipor36@yandex.ru>
2023-05-26 14:40:35 +0500

    5 module (All tasks).

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+
+	let btnArrowRight = document.querySelector('.carousel__arrow_right');
+	let btnArrowLeft = document.querySelector('.carousel__arrow_left');
+	let carousel = document.querySelector('.carousel__inner');
+	let slide = document.querySelector('.carousel__slide');
+	let slideWidth = slide.offsetWidth;
+	let position = 0;
+
+	function makeSlideRight() {
+
+		position += slideWidth;
+		carousel.style.transform = `translateX(-${position}px)`;
+		hideBtn();
+	};
+
+	function makeSlideLeft() {
+		position -= slideWidth;
+		carousel.style.transform = `translateX(-${position}px)`;
+		hideBtn();
+	}
+
+	function hideBtn() {
+
+		let slideAll = document.querySelectorAll('.carousel__slide');
+		(position === slideWidth * (slideAll.length - 1)) ? btnArrowRight.style.display = 'none' : btnArrowRight.style.display = null;
+		position === 0 ? btnArrowLeft.style.display = 'none' : btnArrowLeft.style.display = null;
+	}
+
+	btnArrowLeft.style.display = 'none';
+
+	btnArrowRight.addEventListener('click', makeSlideRight);
+	btnArrowLeft.addEventListener('click', makeSlideLeft);
 }
