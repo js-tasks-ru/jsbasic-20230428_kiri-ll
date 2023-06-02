e848bee3335d1e5935b26f70761a8d0239121067
Kirill Porozov <kipor36@yandex.ru>
2023-06-02 16:27:23 +0500

    6 module 1 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,56 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
-  }
+	elem = null;
+	#items = '';
+
+	constructor(rows) {
+
+		this.#items = rows;
+		this.elem = this.#render();
+	}
+
+	#render() {
+		let wrapTable = document.createElement('div');
+		wrapTable.classList.add('wrapTable');
+		wrapTable.innerHTML = this.#template();
+
+		wrapTable.addEventListener('click', this.#onRemoveRow);
+
+		return wrapTable;
+	}
+
+	#template() {
+		return `
+		<table>
+			<thead>
+			<tr>
+				<th>Имя</th>
+				<th>Возраст</th>
+				<th>Зарплата</th>
+				<th>Город</th>
+				<th></th>
+			</tr>
+			</thead>
+			<tbody>
+				${this.#items.map((elem) => {
+			let row = '';
+			for (let key in elem) {
+				row += `<td>${elem[key]}</td>`;
+			}
+			return row;
+		}).map((elem) => {
+			let row = elem + `<td><button>X</button></td>`;
+			return `<tr class="rowRemove">${row}</tr>`;
+		}).join('')}
+			</tbody>
+		</table>
+		`
+	}
+
+	#onRemoveRow = (event) => {
+		if (event.target.tagName !== 'BUTTON') return;
+		event.target.closest('.rowRemove').remove();
+	}
+
 }
